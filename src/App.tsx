18484933f72456340f1c86731d0b3b3db6ec611b
Viktor Pele <vikpelle@gmail.com>
2019-12-20 19:18:51 +0100

    update App.tsx & remove call

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useContext, FC } from 'react';
 import './App.css';
-import Call from './features/call/Call';
 import { Provider } from 'react-redux';
 import store from './shared/store';
+import { Box, Grommet, ResponsiveContext, Heading, Button } from "grommet";
+import AppBar from './ui/AppBar'
+import NewsComponent from './features/news/ui/News';
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px"
+    }
+  }
+};
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="">
-        <Call />
-      </div>
+      <Grommet theme={theme}>
+        <Box fill>
+          <NewsComponent />
+        </Box>
+      </Grommet>
     </Provider>
   );
 }
